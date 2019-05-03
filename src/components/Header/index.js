d0f4086e28916ef290008f5e2a3f435b9924df2c
willianpassarelli <devpassarelli@gmail.com>
2019-05-03 16:13:22 -0300

    nubank-interface

---
+++ src/components/Header/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from '~/assets/Nubank_Logo.png';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import {
+  Container, Top, Logo, Title,
+} from './styles';
+
+export default function Header() {
+  return (
+    <Container>
+      <Top>
+        <Logo source={logo} />
+        <Title>Willian</Title>
+      </Top>
+      <Icon name="keyboard-arrow-down" size={20} color="#FFF" />
+    </Container>
+  );
+}
